28ffc7f8e2c20629c3edae295665a994b78a8a5b
Bamboobies <80231661+Bamboobies@users.noreply.github.com>
2025-02-03 12:21:42 -0600

    Update flappygame.js

--- flappygame.js
+++ flappygame.js
@@ -35,6 +35,10 @@ function create() {
   bird.body.setCollideWorldBounds(true);
   bird.body.allowGravity = false;
 
+  // Create a BitmapMask for the bird
+  const birdMask = this.make.sprite({ key: 'bird', add: false }).setScale(0.09);
+  bird.mask = new Phaser.Display.Masks.BitmapMask(this, birdMask);
+
   pipes = this.physics.add.group();
   scoreZones = this.physics.add.group();
 
@@ -58,7 +62,7 @@ function create() {
     if (gameOver) return;
 
     pipes.getChildren().forEach(pipe => {
-      if (checkPixelCollision(bird, pipe)) {
+      if (this.physics.overlap(bird, pipe)) {
         hitPipe.call(this);
       }
     });
@@ -173,31 +177,4 @@ function restartGame() {
   this.physics.resume();
   gameOverText.setText('');
   restartText.setText('');
-}
-
-function checkPixelCollision(sprite1, sprite2) {
-  const ctx = game.context; // Use a temporary canvas context for pixel checks
-
-  // Draw sprite1 onto the canvas
-  ctx.clearRect(0, 0, game.scale.width, game.scale.height);
-  ctx.save();
-  ctx.globalCompositeOperation = 'source-over';
-  ctx.drawImage(sprite1.texture.getSourceImage(), sprite1.x - sprite1.width / 2, sprite1.y - sprite1.height / 2, sprite1.width, sprite1.height);
-  ctx.restore();
-
-  // Draw sprite2 onto the canvas
-  ctx.save();
-  ctx.globalCompositeOperation = 'destination-in';
-  ctx.drawImage(sprite2.texture.getSourceImage(), sprite2.x - sprite2.width / 2, sprite2.y - sprite2.height / 2, sprite2.width, sprite2.height);
-  ctx.restore();
-
-  // Check for overlapping non-transparent pixels
-  const imageData = ctx.getImageData(0, 0, game.scale.width, game.scale.height);
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    if (imageData.data[i + 3] > 0) { // Check alpha channel
-      return true; // Collision detected
-    }
-  }
-
-  return false; // No collision
 }
