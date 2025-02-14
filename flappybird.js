d6779cb19e5d0cfa8c52ba5165792d6294602e39
Bamboobies <80231661+Bamboobies@users.noreply.github.com>
2025-02-13 18:20:33 -0600

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -155,13 +155,24 @@ function addPipes() {
   let pipeBottomBody = this.add.sprite(gameWidth, gapY + PIPE_GAP + PIPE_CAP_HEIGHT, 'pipeTexture').setOrigin(0, 0);
   pipeBottomBody.setDisplaySize(PIPE_WIDTH, gameHeight - (gapY + PIPE_GAP)); // Scale to fit the gap
 
-  // Create the top pipe cap
-  let pipeTopCap = this.add.rectangle(gameWidth + PIPE_WIDTH / 2, gapY, PIPE_WIDTH + 10, PIPE_CAP_HEIGHT, 0x006600).setOrigin(0.5, 1);
-  pipeTopCap.setDepth(5);
+  // Create a graphics object for the pipe cap texture
+  const pipeCapTexture = this.make.graphics({ x: 0, y: 0, add: false });
 
-  // Create the bottom pipe cap
-  let pipeBottomCap = this.add.rectangle(gameWidth + PIPE_WIDTH / 2, gapY + PIPE_GAP, PIPE_WIDTH + 10, PIPE_CAP_HEIGHT, 0x006600).setOrigin(0.5, 0);
-  pipeBottomCap.setDepth(5);
+  // Draw a gradient texture for the pipe caps
+  pipeCapTexture.fillGradientStyle(0x004400, 0x006600, 0x005500, 0x007700, 1);
+  pipeCapTexture.fillRect(0, 0, PIPE_WIDTH + 10, PIPE_CAP_HEIGHT); // Fill the cap area
+
+  // Generate a texture from the graphics object
+  pipeCapTexture.generateTexture('pipeCapTexture', PIPE_WIDTH + 10, PIPE_CAP_HEIGHT);
+  pipeCapTexture.destroy(); // Clean up the graphics object
+
+  // Create the top pipe cap with texture
+  let pipeTopCap = this.add.sprite(gameWidth + PIPE_WIDTH / 2, gapY, 'pipeCapTexture').setOrigin(0.5, 1);
+  pipeTopCap.setDisplaySize(PIPE_WIDTH + 10, PIPE_CAP_HEIGHT); // Scale to fit the cap
+
+  // Create the bottom pipe cap with texture
+  let pipeBottomCap = this.add.sprite(gameWidth + PIPE_WIDTH / 2, gapY + PIPE_GAP, 'pipeCapTexture').setOrigin(0.5, 0);
+  pipeBottomCap.setDisplaySize(PIPE_WIDTH + 10, PIPE_CAP_HEIGHT); // Scale to fit the cap
 
   // Create the score zone
   let scoreZone = this.add.rectangle(gameWidth + PIPE_WIDTH / 2, gapY + PIPE_GAP / 2, 10, PIPE_GAP, 0xff0000, 0).setOrigin(0.5);
